bb7e64eb8fc1b3e884be501eda7c3b073a770879
evoxalien <crazzy.john@gmail.com>
2014-04-07 04:49:50 -0500

    Contemplating an Effects Manager

---
+++ Comatose/Comatose/Effects.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Comatose
+{
+    class Effects
+    {
+        class Fire : PhysicsObject
+        {
+            
+            void Create(float x, float y)
+            {
+
+            }
+
+            void Move(float x, float y)
+            {
+
+            }
+
+            void Remove()
+            {
+
+            }
+        }
+    }
+}
