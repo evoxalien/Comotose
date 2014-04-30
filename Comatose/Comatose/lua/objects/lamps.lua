dc65ba364e0dc5fbc8320a59acaaf5764599a615
Nicholas Flynt <thegamefreak0134@gmail.com>
2014-04-30 03:01:17 -0500

    Floor Lamps
    They're neat. I'm tired. Commit logs are for caffinated people.

---
+++ Comatose/Comatose/lua/objects/lamps.lua
@@ -0,0 +1,58 @@
+﻿
+
+FloorLamp = inherits(PhysicsObject)
+
+function FloorLamp:init()
+	self:sprite("Temp-Lamp")
+	self.centered = true
+	self:body_type("static")
+	self:shape("circle")
+	self.cast_shadow = false
+	self.z_index = 0.5
+
+	--item bubble for switch tooltip
+	self.title = ItemBubble.create()
+	self.title:text("Switch")
+	self.title.target = self
+	self.title.centered = true
+
+	--light source
+	self.light = LightSource.create()
+	self.light.ray_length = 50
+	self.on = true
+
+	--"switch" noise
+	self.SwitchSound=Audio.create()
+	self.SwitchSound:audioname("FlashlightOn00")
+	self.SwitchSound:attach(self:ID())
+end
+
+function FloorLamp:everyFrame()
+	if stage.hero then
+		self.SwitchSound:attachListener(stage.hero:ID())
+	end
+	self.light.x = self.x
+	self.light.y = self.y
+end
+
+function FloorLamp:use()
+	distance = stage.hero:distanceFrom(self.x, self.y)
+	if distance <= 15 then
+		if self.on then
+			self.on = false
+			self.light.ray_length = 1
+			self.light:color(255,255,255,0)
+			self.SwitchSound:Play()
+		else
+			self.on = true
+			self.light.ray_length = 50
+			self.light:color(255,255,255,255)
+			self.SwitchSound:Play()
+		end
+	end
+end
+
+registered_objects["FloorLamp"] = {
+	art="Temp-Lamp",
+	centered=true
+}
