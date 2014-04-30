e4fccc0892ca507815c0d4a1f91edbec9e1b27f1
Nicholas Flynt <thegamefreak0134@gmail.com>
2014-04-30 06:07:19 -0500

    Sprinklers! Yay!
    I hate water. Especially WET water! -Pyro

---
+++ Comatose/Comatose/lua/objects/sprinkler.lua
@@ -0,0 +1,28 @@
+﻿
+
+--It's pretty! That's... all it does for now.
+
+Sprinkler = inherits(PhysicsObject)
+
+function Sprinkler:init()
+	self:shape("none")
+	self:body_type("static")		
+	self.on = false
+end
+
+function Sprinkler:switch(trigger)
+	if trigger == self.control then
+		self.on = not self.on
+	end
+end
+
+function Sprinkler:everyFrame()
+	if self.on then
+		Effect:CreateSprinkler(self.x, self.y, 10)
+	end
+end
+
+registered_objects["Sprinkler"] = {
+	art="sprinkler_icon",
+	centered=true
+}
